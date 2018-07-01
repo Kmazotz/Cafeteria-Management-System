f2925cb374a2cb584d1d9e7270e825fbd6acee0f
dolaya237 <dolaya237@hotmail.com>
2018-07-01 13:26:35 -0500

    Project structure

--- Cafeteria Management App/Presentation/src/App.java
+++ Cafeteria Management App/Presentation/src/App.java
@@ -1,41 +1,17 @@
-import Entities.Product;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
 
 public class App extends Application
 {
 
     public static void main(String[] args)
     {
-
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException
+    public void start(Stage primaryStage)
     {
 
-        Stage stage = FXMLLoader.load(getClass().getResource("Inventory.fxml"));
-        stage.show();
-
     }
-
-
 }
